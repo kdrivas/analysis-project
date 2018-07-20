15791515d29fbd8d819237c9501ddd3574968afa
kervyrivas <k.rivas@pucp.pe>
2018-07-20 12:19:40 -0500

    adding new characteristics

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,12 @@ import numpy as np
 import pandas as pd
 import time, pprint    
 
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 pp = pprint.PrettyPrinter(indent=3)
+
+EARLY_MORNING = 21600
+MORNING = 43200      
+AFTERNOON = 64800
 
 def get_week_labels(n_weeks, year, month, day):
     """
@@ -76,26 +80,24 @@ def make_set(train, test, fill_method='median', date_field='Fecha_Ejec_Inicio_In
     val = {}
     
     if fill_method == 'mean':
-        temp_group = train.loc[train.Mxrc == 0].groupby([date_field, 'Id_Job', 'Id_Malla']).mean().reset_index()
+        temp_group = train.loc[train.Mxrc == 0].groupby([date_field, 'Id_Job', 'Id_Malla', 'Hora_Ejec_Inicio_Int']).mean().reset_index()
     elif fill_method == 'median':
-        temp_group = train.loc[train.Mxrc == 0].groupby([date_field, 'Id_Job', 'Id_Malla']).median().reset_index()
+        temp_group = train.loc[train.Mxrc == 0].groupby([date_field, 'Id_Job', 'Id_Malla', 'Hora_Ejec_Inicio_Int']).median().reset_index()
     else:
         print('Error: fill_method should be mean or median')
         return null
     
     date = sorted(train[date_field].unique())[0]
-    # This code fill [Id_Job, Id_Malla] in val. We set as 1 in Time field
-    for ix, row in test.iterrows():
-        val[(date, row['Id_Job'], row['Id_Malla'])] = 0
+    # This code fill [Id_Job, Id_Malla] in val. We set as 0 in Time field
+    # for ix, row in test.iterrows():
+    #     val[(date, row['Id_Job'], row['Id_Malla'])] = 0
     
     for ix, row in temp_group.sort_values([order_field, int_field], ascending=ascending).iterrows():
-        # val[(date, row['Id_Job'], row['Id_Malla'])] = [row[int_field], row['Mxrc'], row['Maxcmpc']]
-        val[(date, row['Id_Job'], row['Id_Malla'])] = row[int_field]
+        val[(date, row['Id_Job'], row['Id_Malla'], row['Hora_Ejec_Inicio_Int'])] = row[int_field]
         
     val = pd.DataFrame(pd.Series(val)).reset_index()
-    val.columns = ['Fecha_Ejec_Inicio_Int', 'Id_Job', 'Id_Malla', int_field]
-    # val[int_field], val['Mxrc'], val['Maxcmpc'] = zip(*val['temp'])
-    # del val['temp']
+    val.columns = ['Fecha_Ejec_Inicio_Int', 'Id_Job', 'Id_Malla', 'Hora_Ejec_Inicio_Int', int_field]
+
     return val
 
 def apply_cats(df, cat_cols):
@@ -128,11 +130,17 @@ def add_date_diffs(day_df, all_data):
     """
         Adds datediffs features to a dataset
     """
-    all_data = all_data[all_data.Mxrc == 0]
-    date = sorted(day_df['Fecha_Ejec_Inicio_Int'].unique())[0]
-    all_data = all_data.sort_values('Fecha_Ejec_Inicio_Int', ascending=False)
+    temp_date = sorted(day_df['Fecha_Ejec_Inicio_Int'].unique())[0]
+    day = int(temp_date % 100)
+    month = int(int(temp_date / 100) % 100)
+    year = int(temp_date / 10000)
+    start_date = int((date(year, month, day) - timedelta(days=7)).strftime('%Y%m%d'))
     
-    day_df['DaysSinceMainframeOp'] = days_since(day_df, all_data.loc[all_data['Fecha_Ejec_Inicio_Int'] < date].reset_index(), ['Id_Job', 'Id_Malla'])
+    temp_data = all_data[all_data.Mxrc == 0]
+    day_df['DaysSinceMainframeOp'] = days_since(day_df, temp_data.loc[temp_data['Fecha_Ejec_Inicio_Int'] < temp_date].reset_index(), ['Id_Job', 'Id_Malla'])
+    
+    temp_data = all_data[all_data.Mxrc != 0]
+    day_df['DaysSinceMainframeOpFail'] = days_since(day_df, temp_data.loc[temp_data['Fecha_Ejec_Inicio_Int'] < temp_date].reset_index(), ['Id_Job', 'Id_Malla'])
 
 def days_count(day_df, all_data, keys):
     """
@@ -148,10 +156,17 @@ def add_days_count(day_df, all_data):
         Adds dayscount features to a dataset (representing a single day/week)
         from the information of trades
     """
-    all_data = all_data[all_data.Mxrc == 0]
-    date = sorted(day_df['Fecha_Ejec_Inicio_Int'].unique())[0]
+    temp_date = sorted(day_df['Fecha_Ejec_Inicio_Int'].unique())[0]
+    day = int(temp_date % 100)
+    month = int(int(temp_date / 100) % 100)
+    year = int(temp_date / 10000)
+    start_date = int((date(year, month, day) - timedelta(days=7)).strftime('%Y%m%d'))
+
+    temp_data = all_data[all_data.Mxrc == 0]
+    day_df['DaysCountMainframeOp'] = days_count(day_df, temp_data.loc[temp_data['Fecha_Ejec_Inicio_Int'] < temp_date].reset_index(), ['Id_Job', 'Id_Malla'])
     
-    day_df['DaysCountMainframeOp'] = days_count(day_df, all_data.loc[all_data['Fecha_Ejec_Inicio_Int'] < date].reset_index(), ['Id_Job', 'Id_Malla'])
+    temp_data = all_data[all_data.Mxrc != 0]
+    day_df['DaysCountMainframeOpFail'] = days_count(day_df, temp_data.loc[temp_data['Fecha_Ejec_Inicio_Int'] < temp_date].reset_index(), ['Id_Job', 'Id_Malla'])
 
 def get_day(row_date):
     if np.isnan(row_date):
@@ -165,48 +180,139 @@ def get_day(row_date):
 def add_date_features(day_df):
     day_df['DiaSemana'] = day_df.apply(lambda row: get_day(row['Fecha_Ejec_Inicio_Int']), axis=1)
 
-def add_mean_days(df, all_data, start_date):
-    
-    date_df = sorted(df['Fecha_Ejec_Inicio_Int'].unique())[0]
-    number_day = get_day(date_df)
+def add_median_days(df, all_data, start_date, actual_date, target_column, name_column, keys):
     
     temp = all_data.loc[(all_data['Fecha_Ejec_Inicio_Int'] >= start_date) &\
-                        (all_data['Fecha_Ejec_Inicio_Int'] < date_df) &\
-                        (all_data['DiaSemana'] == number_day) & (all_data['Mxrc'] == 0)]\
-                        .groupby(['Id_Job', 'Id_Malla']).median().reset_index()
-    dict_temp = {}
-    for ix, row in temp.iterrows():
-        dict_temp[(row['Id_Job'], row['Id_Malla'])] = row['duracion_int']
-    
-    df['promedio_por_dia'] = 0
-    for ix, row in df.iterrows():
-        if (row['Id_Job'], row['Id_Malla']) in dict_temp:
-            df.at[ix, 'promedio_por_dia'] = dict_temp[(row['Id_Job'], row['Id_Malla'])]
+                        (all_data['Fecha_Ejec_Inicio_Int'] < actual_date)]\
+                        .groupby(keys).mean()[target_column].to_dict()
 
-def add_mean_weeks(df, all_data, start_date):
-    date_df = sorted(df['Fecha_Ejec_Inicio_Int'].unique())[0]
-    temp = all_data.loc[(all_data['Fecha_Ejec_Inicio_Int'] >= start_date) &\
-                        (all_data['Fecha_Ejec_Inicio_Int'] < date_df) &\
-                        (all_data['Mxrc'] == 0)]\
-                        .groupby(['Id_Job', 'Id_Malla']).median().reset_index()       
-    dict_temp = {}
-    for ix, row in temp.iterrows():
-        dict_temp[(row['Id_Job'], row['Id_Malla'])] = row['duracion_int']
-    
-    df['promedio_por_semana'] = 0
+    df[name_column] = np.nan
     for ix, row in df.iterrows():
-        if (row['Id_Job'], row['Id_Malla']) in dict_temp:
-            df.at[ix, 'promedio_por_semana'] = dict_temp[(row['Id_Job'], row['Id_Malla'])]
+        if tuple(row[keys]) in temp:
+            df.at[ix, name_column] = temp[tuple(row[keys].values)]
 
-def add_mean_features(df, all_data, n_week):
+def add_median_features(df, all_data, n_week):
+    
     temp_date = sorted(df.Fecha_Ejec_Inicio_Int.unique())[0]
     day = int(temp_date % 100)
     month = int(int(temp_date / 100) % 100)
     year = int(temp_date / 10000)
     start_date = int((date(year, month, day) - timedelta(days=7*(n_week))).strftime('%Y%m%d'))
     
-    add_mean_days(df, all_data, start_date)
-    add_mean_weeks(df, all_data, start_date)
+    actual_date = sorted(df['Fecha_Ejec_Inicio_Int'].unique())[0]
+    number_day = get_day(actual_date)
+
+    temp_all_data_day = all_data.loc[(all_data['DiaSemana'] == number_day) & (all_data['Mxrc'] == 0)]
+    temp_all_data_week = all_data.loc[(all_data['Mxrc'] == 0)]
+    
+    # n_week weeks/months ago date features
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_dia', keys=['Id_Job', 'Id_Malla'])
+    
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_semana', keys=['Id_Job', 'Id_Malla'])
+    
+    start_date = int((date(year, month, day) - timedelta(days=7*4*(n_week))).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_mes_dia', keys=['Id_Job', 'Id_Malla'])
+    
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_mes_semana', keys=['Id_Job', 'Id_Malla'])
+    
+    # one day ago
+    if number_day - 1 < 0:
+        number_day = 7
+    else:
+        number_day -= 1
+    start_date = int((date(year, month, day) - timedelta(days=7)).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_semana_anterior', keys=['Id_Job', 'Id_Malla'])
+    
+    # Last week ago date features
+    start_date = int((date(year, month, day) - timedelta(days=5)).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_dia_anterior', keys=['Id_Job', 'Id_Malla'])
+
+def add_embeds_features(df, model):
+
+    uid = model.trainset._raw2inner_id_users
+    iid = model.trainset._raw2inner_id_items
+
+    # Adding bias features
+    df['id_job_bias'] = df.Id_Job.apply(lambda row: model.bu[uid[row]])
+    df['id_mesh_bias'] = df.Id_Malla.apply(lambda row: model.bi[iid[row]])
+    df['fail_bias'] = df.apply(lambda row: model.predict(row['Id_Job'], row['Id_Malla'])[3], axis=1)
+
+    # Adding factors features
+    fact_job = np.array([model.pu[uid[cIdx]] for cIdx in df.Id_Job])
+    fact_mesh = np.array([model.qi[iid[iIdx]] for iIdx in df.Id_Malla])
+    for ixf in range(fact_job.shape[1]):
+        df[f'f_job_{ixf}']  = fact_job[:, ixf]
+        df[f'f_mesh_{ixf}']  = fact_mesh[:, ixf]
+        
+def decide_time(vals):
+
+    if vals < EARLY_MORNING:
+        return 0
+    elif vals < MORNING:
+        return 1
+    elif vals < AFTERNOON:
+        return 2
+    else:
+        return 3
+        
+def add_hour_features(df, all_data, n_week):
+    
+    # 0->madrugada, 1->mañana, 2->tarde, 3->noche
+    df['tiempo'] = df.apply(lambda row: decide_time(row['Hora_Ejec_Inicio_Int']), axis=1)
+    
+    actual_date = sorted(df['Fecha_Ejec_Inicio_Int'].unique())[0]
+    day = int(actual_date % 100)
+    month = int(int(actual_date / 100) % 100)
+    year = int(actual_date / 10000)
+    start_date = int((date(year, month, day) - timedelta(days=7*(n_week))).strftime('%Y%m%d'))
+    
+    number_day = get_day(actual_date)
+    
+    temp_all_data_day = all_data.loc[(all_data['DiaSemana'] == number_day) & (all_data['Mxrc'] == 0)]
+    temp_all_data_week = all_data.loc[(all_data['Mxrc'] == 0)]
+    
+    # n_week weeks/months ago date features
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_hora_dia', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_hora_semana', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+    
+    start_date = int((date(year, month, day) - timedelta(days=7*4*(n_week))).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_hora_mes_dia', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_hora_mes_semana', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+    
+    # one day ago
+    if number_day - 1 < 0:
+        number_day = 7
+    else:
+        number_day -= 1
+    start_date = int((date(year, month, day) - timedelta(days=7)).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='duracion_int', name_column='promedio_hora_por_semana_anterior', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+    
+    # Last week ago date features
+    start_date = int((date(year, month, day) - timedelta(days=5)).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_week, start_date, actual_date, target_column='duracion_int', name_column='promedio_por_hora_dia_anterior', keys=['Id_Job', 'Id_Malla', 'tiempo'])
+
+def add_volume_jobs_features(df, all_data, n_week=3):
+    volume_jobs = all_data.groupby(['Fecha_Ejec_Inicio_Int', 'Id_Malla', 'tiempo']).count().reset_index()
+    volume_jobs['DiaSemana'] = volume_jobs.apply(lambda row: get_day(row['Fecha_Ejec_Inicio_Int']), axis=1)
+
+    actual_date = sorted(df.Fecha_Ejec_Inicio_Int.unique())[0]
+    day = int(actual_date % 100)
+    month = int(int(actual_date / 100) % 100)
+    year = int(actual_date / 10000)
+    start_date = int((date(year, month, day) - timedelta(days=7*(n_week))).strftime('%Y%m%d'))
+    
+    number_day = get_day(actual_date)
+    
+    temp_all_data_day = volume_jobs.loc[(all_data['DiaSemana'] == number_day)]
+    
+    # n_week weeks/months ago date features
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='Id_Job', name_column='volumen_por_hora_dia', keys=['Id_Malla', 'tiempo'])
+    add_median_days(df, volume_jobs, start_date, actual_date, target_column='Id_Job', name_column='volumen_por_hora_semana', keys=['Id_Malla', 'tiempo'])
+    
+    start_date = int((date(year, month, day) - timedelta(days=7*4*(n_week))).strftime('%Y%m%d'))
+    add_median_days(df, temp_all_data_day, start_date, actual_date, target_column='Id_Job', name_column='volumen_por_hora_mes_dia', keys=['Id_Malla', 'tiempo'])
+    add_median_days(df, volume_jobs, start_date, actual_date, target_column='Id_Job', name_column='volumen_por_hora_mes_semana', keys=['Id_Malla', 'tiempo'])
+    
     
 def fit_model(model, X_trn, y_trn, X_val, y_val, early_stopping, cat_indices):
     if X_val is not None:
@@ -214,12 +320,12 @@ def fit_model(model, X_trn, y_trn, X_val, y_val, early_stopping, cat_indices):
         model.fit(X_trn, y_trn, 
                 eval_set=[(X_val, y_val)],
                 early_stopping_rounds=early_stopping,
-                eval_metric='mse')
+                eval_metric='mae')
     else:
         model.fit(X_trn, y_trn)
         
 def calculate_metrics(model, metrics, X_trn, y_trn, X_val, y_val):
-    metric_function = {'mse': mean_squared_error}
+    metric_function = {'mse': mean_squared_error, 'mae': mean_absolute_error}
     dset = {'trn': {'X': X_trn, 'y': y_trn},
             'val': {'X': X_val, 'y': y_val}}
     
